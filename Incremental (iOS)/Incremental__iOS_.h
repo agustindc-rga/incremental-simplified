e92b8d279bdc48cf8a46cd809392f06ed5e8e5c3
Agustín de Cabrera <Agustin.DeCabrera@rga.com>
2017-11-15 20:19:03 -0300

    Added target for iOS

---
+++ Incremental (iOS)/Incremental__iOS_.h
@@ -0,0 +1,19 @@
+//
+//  Incremental__iOS_.h
+//  Incremental (iOS)
+//
+//  Created by Agustin De Cabrera on 13/11/17.
+//  Copyright © 2017 objc.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Incremental__iOS_.
+FOUNDATION_EXPORT double Incremental__iOS_VersionNumber;
+
+//! Project version string for Incremental__iOS_.
+FOUNDATION_EXPORT const unsigned char Incremental__iOS_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Incremental__iOS_/PublicHeader.h>
+
+
