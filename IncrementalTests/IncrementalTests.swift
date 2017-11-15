f81757792a67011361f234777f7735453f691209
Agustín de Cabrera <Agustin.DeCabrera@rga.com>
2017-11-15 20:20:54 -0300

    Added Unit tests target

---
+++ IncrementalTests/IncrementalTests.swift
@@ -0,0 +1,13 @@
+//
+//  IncrementalTests.swift
+//  IncrementalTests
+//
+//  Created by Agustin De Cabrera on 13/11/17.
+//  Copyright © 2017 objc.io. All rights reserved.
+//
+
+import XCTest
+import Incremental
+
+class IncrementalTests: XCTestCase {
+}
